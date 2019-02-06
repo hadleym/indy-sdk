4a31e13e457ec24054872af6b0a5dcd3cb203fca
Mark Hadley <mark.hadley@evernym.com>
2019-02-06 07:18:45 -0700

    Messages During Post

--- vcx/libvcx/src/utils/httpclient.rs
+++ vcx/libvcx/src/utils/httpclient.rs
@@ -19,7 +19,6 @@ pub fn post_u8(body_content: &Vec<u8>) -> Result<Vec<u8>,String> {
         info!("::Android code");
         set_ssl_cert_location();
     }
-    let client = reqwest::ClientBuilder::new().timeout(None).build().or(Err("Preparing Post failed".to_string()))?;
     println!("Posting encrypted bundle to: \"{}\"", url);
     let mut message = client.post(&url).body(body_content.to_owned()).header(CONTENT_TYPE, "octet_stream");
 
